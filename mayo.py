aa7e63a4f8ba6bdca13d8cd379da4d883fc1c2e1
joseamarcucci <74363457+joseamarcucci@users.noreply.github.com>
2021-05-10 16:50:49 -0300

    Update mayo.py

--- mayo.py
+++ mayo.py
@@ -103,7 +103,7 @@ def app():
     
 
 
-    if st.checkbox('Usuarios x día'):
+    if st.checkbox('Usuarios totales x día'):
        
        st.table(usuarios[['NameOfAttendee','AttendeeFirstJoinTime']])
        st.line_chart(usuarios)
