bfb03c313c043a7c065e10dd5dcfef11624af434
joseamarcucci <74363457+joseamarcucci@users.noreply.github.com>
2021-07-05 10:14:51 -0300

    Update tjunio30.py

--- tjunio30.py
+++ tjunio30.py
@@ -130,7 +130,7 @@ def app():
     aulast=len(times3t)
     #buff.write(round(timesu,))
     st.sidebar.write('Minutos/mes: ',round(timesu,0))
-    st.sidebar.write('Minutos/totales: ',6569440+round(timesu,0))
+    st.sidebar.write('Minutos/totales: ',48269353+round(timesu,0))
     df8 = pd.DataFrame({
  
   
